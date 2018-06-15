de282f07358821b13ee4056def70894b69dda380
nikhil914 <nikhilranjan914@gmail.com>
2018-06-16 03:09:40 +0530

    initial commit

--- class 4/Even.java
+++ class 4/Even.java
@@ -1,16 +1,14 @@
 import java.util.Scanner;
 class Even{
 	public static void main(String[] args) {
-
+		
 		Scanner in=new Scanner(System.in);
-		System.out.println("Enter No:");
+		System.out.print("Enter No:");
 		int no=in.nextInt();
-		for(int i=no;i>=0;i--){
-			if (no%2==0) {
-				System.out.println(no +" is Even");}
-				else{
-					System.out.println(no +" is Odd");
-				}
+		for(int i=0;i<=no;i++){
+			if (i%2==0) {
+				System.out.print(i +" ");}
+				
 			
 		}
 		
