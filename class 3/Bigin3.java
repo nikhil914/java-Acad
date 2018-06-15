6d31f5a37974d6a417a0a205c64afaa26e11548f
nikhil914 <nikhilranjan914@gmail.com>
2018-06-16 02:34:03 +0530

    Initial commit

---
+++ class 3/Bigin3.java
@@ -0,0 +1,17 @@
+class Bigin3{
+	public static void main(String[] args) {
+		
+	
+	int a=1,b=2,c=3;
+	if ((a>b)&&(a>c)) {
+		System.out.println("A is greater");
+			}
+			else if ((b>a)&&(b>c)) {
+				System.out.println("B is Greater");
+			}
+			else{
+				System.out.println("C Greater");
+			}
+}
+
+}
